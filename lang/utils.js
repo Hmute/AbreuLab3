7b42a97ff07bb87f53e3afc3ea83ce9f510560bc
Hmute <heraldoabreusound@gmail.com>
2025-09-28 18:23:20 -0700

    first commit

---
+++ lang/utils.js
@@ -0,0 +1,9 @@
+const messages = require('../lang/en');
+
+function getDate(name) {
+    const now = new Date();
+    const msg = messages.greeting.replace("%1", name);
+    return `<p style="color:blue">${msg} ${now}</p>`;
+}
+
+module.exports = { getDate };
---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const url = require('url');
+const utils = require('./modules/utils');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+
+    if (parsedUrl.pathname === "/COMP4537/labs/3/getDate") {
+        const name = parsedUrl.query.name || "Guest";
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(utils.getDate(name));
+    } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Not Found");
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
